309eca0dc8377d41324cc7f20688fa4ea17425eb
jlal1226 <jlal1226@naver.com>
2022-01-01 23:33:56 +0900

    IntArrStack 설명 정리

--- src/chap04/IntAryQueue.java
+++ src/chap04/IntAryQueue.java
@@ -1,5 +1,6 @@
 package chap04;
 
+// 배열 요소를 앞쪽으로 옮기는 큐
 public class IntAryQueue {
     private int max;
     private int num;
@@ -22,11 +23,14 @@ public class IntAryQueue {
             max = 0;
         }
     }
-
+    
+    // 시간 복잡도 O(1)
     public int enQue(int x) throws OverflowIntStackException {
         return que[num++] = x;
     }
-
+    
+    // 시간복잡도 O(n)으로 성능이 좋지 않다
+    // 배열 맨 앞의 요소를 꺼내고 모든 요소를 앞으로 한 칸씩 옮긴다
     public int deQue() throws EmptyIntStackException {
         return que[0];
     }
