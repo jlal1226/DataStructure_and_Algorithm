4b044a0a7d84cf339d823d662d76fd1c5ee2346f
jlal1226 <jlal1226@naver.com>
2022-01-08 22:52:10 +0900

    chap05 팩토리얼 실습

---
+++ src/chap05/Factorial.java
@@ -0,0 +1,22 @@
+package chap05;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    static int factorial(int n){
+        if (n > 0)
+            return n * factorial(n-1);
+        else
+            return 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요 : ");
+        int input = scan.nextInt();
+
+        System.out.println(input+" factorial : " + factorial(input));
+    }
+}
