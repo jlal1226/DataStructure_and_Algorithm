d2e465b4c37d3f6acd906a9223ec6fee523f889d
jlal1226 <jlal1226@naver.com>
2022-01-11 23:19:51 +0900

    chap05 EightQueenUI 초안

---
+++ src/chap05/EightQueenUI.java
@@ -0,0 +1,18 @@
+package chap05;
+
+public class EightQueenUI {
+    static int[] pos = new int[8]; // 열에 퀸을 배치
+    static boolean[] flag_a = new boolean[8]; // 행에 퀸이 배치되었는지 체크
+    static boolean[] flag_b = new boolean[15]; // 오른쪽 위 대각선에 퀸이 배치됬는지 체크
+    static boolean[] flag_c = new boolean[15]; // 오른쪽 아래 대각선엔 퀸이 배치됬는지 체크
+
+    static void print() {
+
+    }
+    static void set(int i) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
