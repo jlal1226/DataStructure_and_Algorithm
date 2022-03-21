e8f79ca3dc11df748aacf39a4b7552adc8d54441
jlal1226 <jlal1226@naver.com>
2022-03-21 15:59:34 +0900

    MergeSort 재구현

--- src/chap06/MergeSort.java
+++ src/chap06/MergeSort.java
@@ -1,65 +1,50 @@
 package chap06;
 
 import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
 
 public class MergeSort {
-    static int[] buff; // 작업용 배열
-    // arr[left] ~ arr[right] 재귀적으로 병합 정렬
+    static int[] buff;
+    static void mergeSort(int[] arr) {
+        int len = arr.length;
+        buff = new int[len];
+        __mergeSort(arr, 0, len - 1);
+    }
+
     static void __mergeSort(int[] arr, int left, int right) {
         if (left < right) {
-            int i;
             int center = (left + right) / 2;
-            int p = 0;
-            int j = 0;
-            int k = left;
-
-            __mergeSort(arr, 0, center); // 앞부분
-            __mergeSort(arr, center + 1, right); // 뒷부분
-
-            // 배열의 앞부분을 buff[]에 복사
-            // p값은 center - left + 1이 된다.
-            for (i = left; i <= center; i++)
-                buff[p++] = arr[i];
-            // 배열의 뒷부분과 buff[]의 요소를 비교하면서 작은 요소를 배열 arr에 병합
-            while ( i <= right && j < p)
-                arr[k++] = (buff[j] <= arr[i]) ? buff[j++] : arr[i++];
-            // buff[]의 나머지 요소를 배열 arr에 복사
-            // 만약  기존 arr[]에 요소가 남아있다면 그냥 둬도 됌 -> 정렬이 되있고 buff[]에 있는 요소보다 큰 값들이기 때문이다.
-            while (j < p)
-                arr[k++] = buff[j++];
+            __mergeSort(arr, left, center);
+            __mergeSort(arr, center + 1, right);
+            merge(arr, left, right);
         }
     }
-    // 병합 정렬
-    static void mergeSort(int[] arr, int n) {
-        buff = new int[n]; // 작업용 배열 생성
-        __mergeSort(arr, 0, n-1); // 병합 정렬
-        buff = null; // 작업용 버퍼 해제
+
+    private static void merge(int[] arr, int left, int right) {
+        int center = (left + right) / 2;
+        int p = 0; // 버퍼 끝부분 커서
+        int b_start = 0; // 버퍼 커서
+        int pa = left; // 배열 a의 커서
+        int pr = center + 1; // 배열의 오른쪽 부분 커서
+
+        // 배열의 왼쪽부분을 버퍼에 저장
+        for (int i = left; i <= center; i++) {
+            buff[p++] = arr[i];
+        }
+
+        // 버퍼와 배열의 오른쪽 부분을 크기 순으로 배열 a에 다시 저장
+        while (b_start < p && pr <= right) {
+            arr[pa++] = (buff[b_start] <= arr[pr]) ? buff[b_start++] : arr[pr++];
+        }
+        // 버퍼에 남아있는 요소를 배열 a로 복사 -> 배열의 오른쪽 부분의 요소가 남아있는건 그대로 두면 됌
+        while (b_start < p) {
+            arr[pa++] = buff[b_start++];
+        }
+
     }
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Random rand = new Random();
-
-        System.out.print("요솟수 : ");
-        int num = scan.nextInt();
-/*        int[] arr = new int[num];
-
-        // 1 ~ 50 랜덤 요소 생성
-        for (int i = 0; i < num; i++){
-            arr[i] = rand.nextInt(50) + 1;
-            for (int j = 0; j < i; j++){
-                if (arr[i] == arr[j]){
-                    i--;
-                    break;
-                }
-            }
-        }*/
-        int[] arr = {1, 3, 12, 6, 4, 11, 8, 7, 3, 2, 13, 16};
-
-
-        System.out.println("정렬 전 : " + Arrays.toString(arr));
-        mergeSort(arr, num);
-        System.out.println("정렬 후 : " + Arrays.toString(arr));
+        int[] arr = {7, 22, 5, 10, 45, 30, 1, 13, 27};
+        mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
